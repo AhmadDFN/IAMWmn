1507584f3abd5fce417da06bccbe4df6286a9851
dasaraul <acmadauliai@example.com>
2023-05-25 12:41:23 +0700

    Mengatur Template

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -5,16 +5,16 @@ scratch. This page gets rid of all links and provides the needed markup only.
 --}}
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <meta content="" name="keywords">
-  <meta content="" name="description">
-  <title>@yield('title') | IAMW WEC - MADIUN</title>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+    <title>@yield('title') | IAMW WEC - MADIUN</title>
 
-  @include('layouts.sc_head')
+@include('layouts.sc_head')
 </head>
 <body>
-  <div class="container-fluid position-relative d-flex p-0">
+<div class="container-fluid position-relative d-flex p-0">
     <!-- Spinner Start -->
     <div id="spinner" class="show bg-dark position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
         <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
@@ -25,46 +25,33 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
     @include('layouts.sidebar')
     
-      <!-- Content Start -->
-      <div class="content">
+    <!-- Content Start -->
+    <div class="content">
         @include('layouts.navbar')
         <!-- Sale & Revenue Start -->
         <div class="container-fluid pt-4 px-4">
-            <div class="row g-4">
-                <div class="col-sm-6 col-xl-3">
-                    <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
-                        <i class="fa fa-chart-line fa-3x text-primary"></i>
-                        <div class="ms-3">
-                            <p class="mb-2">Today Sale</p>
-                            <h6 class="mb-0">$1234</h6>
-                        </div>
+            <div class="row g-0 bg-secondary">
+                <div class="col-sm-4">
+                    <div class="d-flex align-items-center justify-content-between p-4">
+                        <h1 class="m-0">@yield('page-title')</h1>
                     </div>
                 </div>
-                <div class="col-sm-6 col-xl-3">
-                    <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
-                        <i class="fa fa-chart-bar fa-3x text-primary"></i>
-                        <div class="ms-3">
-                            <p class="mb-2">Total Sale</p>
-                            <h6 class="mb-0">$1234</h6>
+                <div class="col-sm-4">
+                    <div class="d-flex align-items-center justify-content-between p-4">
+                    {{-- Notif --}}
+                    @if (session("mess"))
+                        <div class="alert alert-{{ session("type") }} text-center" style="width: 300px;" role="alert">
+                            {{ session("mess") }}
                         </div>
+                    @endif
                     </div>
                 </div>
-                <div class="col-sm-6 col-xl-3">
-                    <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
-                        <i class="fa fa-chart-area fa-3x text-primary"></i>
-                        <div class="ms-3">
-                            <p class="mb-2">Today Revenue</p>
-                            <h6 class="mb-0">$1234</h6>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-xl-3">
-                    <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
-                        <i class="fa fa-chart-pie fa-3x text-primary"></i>
-                        <div class="ms-3">
-                            <p class="mb-2">Total Revenue</p>
-                            <h6 class="mb-0">$1234</h6>
-                        </div>
+                <div class="col-sm-4">
+                    <div class="d-flex align-items-center justify-content-between p-4">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="{{ url('/') }}">Home</a></li>
+                            <li class="breadcrumb-item active">Starter Page</li>
+                        </ol>
                     </div>
                 </div>
             </div>
@@ -97,28 +84,28 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <!-- Sales Chart End -->
 
         <!-- Footer Start -->
-        <div class="container-fluid pt-4 px-4">
+        <div class="container-fluid pt-4 px-4 mt-auto">
             <div class="bg-secondary rounded-top p-4">
                 <div class="row">
                     <div class="col-12 col-sm-6 text-center text-sm-start">
-                        &copy; <a href="#">IAMW</a>, iRzella - All Right Reserved. 
+                        &copy; <a href="https://www.instagram.com/wearnesmadiun/" target="_blank">IAMW</a>, iRzella - All Right Reserved. 
                     </div>
                     <div class="col-12 col-sm-6 text-center text-sm-end">
                         <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-                        Designed By <a href="https://htmlcodex.com">iRzellA</a>
-                        <br>Distributed By: <a href="https://themewagon.com" target="_blank">Ahmad Dany FN</a>
+                        Designed By <a href="https://github.com/AhmadDFN" target="_blank">iRzellA</a>
+                        <br>Distributed By: <a href="https://www.linkedin.com/in/ahmaddfn/" target="_blank">Ahmad Dany FN</a>
                     </div>
                 </div>
             </div>
         </div>
         <!-- Footer End -->
-      </div>
+    </div>
     <!-- Content End -->
 
 
     <!-- Back to Top -->
     <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
-  </div>
+</div>
 @include('layouts.sc_footer')
 
 </body>
